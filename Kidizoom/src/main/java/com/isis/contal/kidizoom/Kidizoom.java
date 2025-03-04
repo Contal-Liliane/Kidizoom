c94bb5a0c2c9cc06d5f5f2e58296d32561246edb
louis <louis@10.187.2.104>
2025-03-04 15:37:25 +0100

    APAGNAN

--- Kidizoom/src/main/java/com/isis/contal/kidizoom/Kidizoom.java
+++ Kidizoom/src/main/java/com/isis/contal/kidizoom/Kidizoom.java
@@ -11,6 +11,6 @@ package com.isis.contal.kidizoom;
 public class Kidizoom {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("APAGNAN!");
     }
 }
