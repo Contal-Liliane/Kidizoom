2151e514c89ead4e04c6f9144e46eec08bc7023b
lilia <lilia@10.187.14.137>
2025-03-04 15:27:19 +0100

    Premier commit

---
+++ Kidizoom/src/main/java/com/isis/contal/kidizoom/Kidizoom.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.isis.contal.kidizoom;
+
+/**
+ *
+ * @author lilia
+ */
+public class Kidizoom {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
